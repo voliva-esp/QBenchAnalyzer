ffc5167b35e461781b1b8544fc4c2fa98c384cf1
romOlivo <al341918@uji.es>
2025-05-09 16:34:25 +0200

    Cleaning

--- build/lib/QBenchAnalyzer/__init__.py
+++
@@ -1,3 +0,0 @@
-from .circuit_analyzer import analyze_circuit_group_structural
-from .circuit_generator import ICircuitGenerator
-from .metrics_generator import generate_metrics
--- build/lib/QBenchAnalyzer/circuit_generator.py
+++
@@ -1,7 +0,0 @@
-class ICircuitGenerator:
-    def __init__(self, name):
-        self.name = name
-
-    def generate_qiskit_circuit(self, number_of_qubits):
-        pass
-
